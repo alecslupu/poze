084977ed227e41b274be657fd5644d8a65fc403b
Alexandru-Emil Lupu <alex.lupu@2parale.ro>
2014-05-10 20:01:52 +0300

    Added lockable si confirmable to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140510164229) do
+ActiveRecord::Schema.define(version: 20140510165028) do
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -28,9 +28,18 @@ ActiveRecord::Schema.define(version: 20140510164229) do
     t.datetime "updated_at"
     t.string   "name"
     t.integer  "role"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.integer  "failed_attempts",        default: 0,  null: false
+    t.string   "unlock_token"
+    t.datetime "locked_at"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["unlock_token"], name: "index_users_on_unlock_token", unique: true, using: :btree
 
 end
